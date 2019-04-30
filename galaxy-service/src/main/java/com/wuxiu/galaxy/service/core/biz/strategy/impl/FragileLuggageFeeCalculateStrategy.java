731eec6aac10b8a827ad8e942df7a6c89486b499
无朽 <wuxiu@maihaoche.com>
2019-04-30 22:21:46 +0800

    add different strategies about feeCalculation

--- galaxy-service/src/main/java/com/wuxiu/galaxy/service/core/biz/strategy/impl/FragileLuggageFeeCalculateStrategy.java
+++ galaxy-service/src/main/java/com/wuxiu/galaxy/service/core/biz/strategy/impl/FragileLuggageFeeCalculateStrategy.java
@@ -1,11 +1,12 @@
 package com.wuxiu.galaxy.service.core.biz.strategy.impl;
 
+import com.wuxiu.galaxy.api.dto.FragileLuggageFeeCalculateParamDTO;
 import com.wuxiu.galaxy.api.dto.LuggageChargeCalculationResultDTO;
 import com.wuxiu.galaxy.api.dto.LuggageFeeBaseCalculationParamDTO;
-import com.wuxiu.galaxy.service.core.biz.strategy.LuggageFeeCalculationStrategy;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,13 +16,30 @@ import java.util.List;
  * @date: 2019/4/22 10:56
  */
 @Slf4j
-public class FragileLuggageFeeCalculateStrategy implements LuggageFeeCalculationStrategy {
+public class FragileLuggageFeeCalculateStrategy extends LuggageFixedFeeCalculationStrategy {
 
     @Override
     public LuggageChargeCalculationResultDTO calculate(
             Integer calculateDays,
             List<LuggageFeeBaseCalculationParamDTO> calculationParamDTOS) {
-        return null;
+
+        LuggageFeeBaseCalculationParamDTO luggageFeeBaseCalculationParamDTO =
+                calculationParamDTOS.get(0);
+
+        // 判断计价参数类型是否符合预期
+        if (!(luggageFeeBaseCalculationParamDTO instanceof
+                FragileLuggageFeeCalculateParamDTO)) {
+            throw new RuntimeException("Param class is " +
+                    luggageFeeBaseCalculationParamDTO.getClass().getSimpleName()
+                    + ", not the expected FragileLuggageFeeCalculateParamDTO.");
+        }
+
+        // 转换类型，开始计算
+        FragileLuggageFeeCalculateParamDTO fragileLuggageFeeCalculateParamDTO =
+                (FragileLuggageFeeCalculateParamDTO) luggageFeeBaseCalculationParamDTO;
+
+        return super.calculate(calculateDays,
+                Collections.singletonList(fragileLuggageFeeCalculateParamDTO));
     }
 
     @Override
@@ -29,6 +47,23 @@ public class FragileLuggageFeeCalculateStrategy implements LuggageFeeCalculation
             LocalDateTime startDate,
             LocalDateTime endDate,
             List<LuggageFeeBaseCalculationParamDTO> calculationParamDTOS) {
-        return null;
+
+        LuggageFeeBaseCalculationParamDTO luggageFeeBaseCalculationParamDTO =
+                calculationParamDTOS.get(0);
+
+        // 判断计价参数类型是否符合预期
+        if (!(luggageFeeBaseCalculationParamDTO instanceof
+                FragileLuggageFeeCalculateParamDTO)) {
+            throw new RuntimeException("Param class is " +
+                    luggageFeeBaseCalculationParamDTO.getClass().getSimpleName()
+                    + ", not the expected FragileLuggageFeeCalculateParamDTO.");
+        }
+
+        // 转换类型，开始计算
+        FragileLuggageFeeCalculateParamDTO fragileLuggageFeeCalculateParamDTO =
+                (FragileLuggageFeeCalculateParamDTO) luggageFeeBaseCalculationParamDTO;
+
+        return super.dailyCalculate(startDate, endDate, Collections.singletonList(
+                fragileLuggageFeeCalculateParamDTO));
     }
 }
--- galaxy-service/src/main/java/com/wuxiu/galaxy/service/core/biz/strategy/impl/ValuableLuggageFeeCalculateStrategy.java
+++ galaxy-service/src/main/java/com/wuxiu/galaxy/service/core/biz/strategy/impl/ValuableLuggageFeeCalculateStrategy.java
@@ -2,10 +2,11 @@ package com.wuxiu.galaxy.service.core.biz.strategy.impl;
 
 import com.wuxiu.galaxy.api.dto.LuggageChargeCalculationResultDTO;
 import com.wuxiu.galaxy.api.dto.LuggageFeeBaseCalculationParamDTO;
-import com.wuxiu.galaxy.service.core.biz.strategy.LuggageFeeCalculationStrategy;
+import com.wuxiu.galaxy.api.dto.ValuableLuggageFeeCalculateParamDTO;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,13 +16,30 @@ import java.util.List;
  * @date: 2019/4/22 10:57
  */
 @Slf4j
-public class ValuableLuggageFeeCalculateStrategy implements LuggageFeeCalculationStrategy {
+public class ValuableLuggageFeeCalculateStrategy extends LuggageFixedFeeCalculationStrategy {
 
     @Override
     public LuggageChargeCalculationResultDTO calculate(
             Integer calculateDays,
             List<LuggageFeeBaseCalculationParamDTO> calculationParamDTOS) {
-        return null;
+
+        LuggageFeeBaseCalculationParamDTO luggageFeeBaseCalculationParamDTO =
+                calculationParamDTOS.get(0);
+
+        // 判断计价参数类型是否符合预期
+        if (!(luggageFeeBaseCalculationParamDTO instanceof
+                ValuableLuggageFeeCalculateParamDTO)) {
+            throw new RuntimeException("Param class is " +
+                    luggageFeeBaseCalculationParamDTO.getClass().getSimpleName()
+                    + ", not the expected ValuableLuggageFeeCalculateParamDTO.");
+        }
+
+        // 转换类型，开始计算
+        ValuableLuggageFeeCalculateParamDTO valuableLuggageFeeCalculateParamDTO =
+                (ValuableLuggageFeeCalculateParamDTO) luggageFeeBaseCalculationParamDTO;
+
+        return super.calculate(calculateDays,
+                Collections.singletonList(valuableLuggageFeeCalculateParamDTO));
     }
 
     @Override
@@ -29,6 +47,23 @@ public class ValuableLuggageFeeCalculateStrategy implements LuggageFeeCalculatio
             LocalDateTime startDate,
             LocalDateTime endDate,
             List<LuggageFeeBaseCalculationParamDTO> calculationParamDTOS) {
-        return null;
+
+        LuggageFeeBaseCalculationParamDTO luggageFeeBaseCalculationParamDTO =
+                calculationParamDTOS.get(0);
+
+        // 判断计价参数类型是否符合预期
+        if (!(luggageFeeBaseCalculationParamDTO instanceof
+                ValuableLuggageFeeCalculateParamDTO)) {
+            throw new RuntimeException("Param class is " +
+                    luggageFeeBaseCalculationParamDTO.getClass().getSimpleName()
+                    + ", not the expected ValuableLuggageFeeCalculateParamDTO.");
+        }
+
+        // 转换类型，开始计算
+        ValuableLuggageFeeCalculateParamDTO valuableLuggageFeeCalculateParamDTO =
+                (ValuableLuggageFeeCalculateParamDTO) luggageFeeBaseCalculationParamDTO;
+
+        return super.dailyCalculate(startDate, endDate, Collections.singletonList(
+                valuableLuggageFeeCalculateParamDTO));
     }
 }
