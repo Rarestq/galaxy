33eb044cf75a3c1b096612a009cbd8e2dacaca7c
无朽 <wuxiu@maihaoche.com>
2019-05-16 17:57:31 +0800

    add dubbo & zk configuration

--- galaxy-service/src/main/java/com/wuxiu/galaxy/service/core/GalaxyServiceApplication.java
+++ galaxy-service/src/main/java/com/wuxiu/galaxy/service/core/GalaxyServiceApplication.java
@@ -1,29 +1,33 @@
-package com.wuxiu.galaxy.web;
+package com.wuxiu.galaxy.service.core;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
 
-
 /**
- * 项目启动类
+ * 服务启动类
  *
- * @author wuxiu
+ * @author: wuxiu
+ * @date: 2019/5/16 16:31
  */
 @ComponentScan(basePackages = {
-        "com.wuxiu.galaxy.integration",
-        "com.wuxiu.galaxy.web"})
+        "com.wuxiu.galaxy.dal",
+        "com.wuxiu.galaxy.service"})
+@MapperScan("com.wuxiu.galaxy.dal.dao")
+@EnableTransactionManagement
 @EnableSwagger2Doc
 @RestController
 @SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class},
         scanBasePackages = "com.wuxiu.galaxy")
-public class GalaxyWebApplication {
+public class GalaxyServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GalaxyWebApplication.class, args);
+        SpringApplication.run(GalaxyServiceApplication.class, args);
     }
 
 }
